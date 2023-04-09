ddb2cc7122d06a5dd3edac1a0988821470ed398d
H1MSK <ksda47832338@outlook.com>
2023-04-09 14:32:10 +0800

    dep: change qt to submodule

--- qt/include/named_value.cpp
+++
@@ -1,9 +0,0 @@
-#include "named_value.hpp"
-#include "value_validator.hpp"
-
-namespace SideAssist::Qt {
-bool NamedValue::validate(const QJsonValue& val) {
-  return validator_ == nullptr ? true : validator_->validate(val);
-}
-
-}  // namespace SideAssist::Qt
--- qt/todo.md
+++
@@ -1,1 +0,0 @@
-- [ ] 仅订阅已有的option的/{name}及/{name}/set, 并在!=Undefined时清除/{name}订阅
